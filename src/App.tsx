7bb88573abb42c0875c7ed4471fa24c36837d2f2
Rebecca <rebecca@croysdale.net>
2024-09-11 11:21:03 -0700

    Fixes file name

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { Navbar } from './components/Navbar'
 import { PostsMainPage } from './features/posts/PostsMainPage'
-import { SinglePostPage } from './features/posts/SinglePostsPage'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
 
 function App() {
