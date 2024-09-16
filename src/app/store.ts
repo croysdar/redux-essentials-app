3d8f43a192d31f7e677d9e92cb2a4fa3e2a20df9
Rebecca <rebecca@croysdale.net>
2024-09-16 14:52:07 -0700

    Injects get users endpoint into apiSlice within usersSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,6 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
 
 import postsReducer from '@/features/posts/postsSlice'
-import usersReducer from '@/features/users/usersSlice'
 import authReducer from '@/features/auth/authSlice'
 import notificationsReducer from '@/features/notifications/notificationsSlice'
 import { listenerMiddleware } from './listenerMiddleware'
@@ -13,7 +12,6 @@ export const store = configureStore({
     reducer: {
         auth: authReducer,
         posts: postsReducer,
-        users: usersReducer,
         notifications: notificationsReducer,
         [apiSlice.reducerPath]: apiSlice.reducer
     },
--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,8 +1,7 @@
 import { RootState } from '@/app/store'
-import { createAppSlice } from '@/app/hooks'
-import { client } from '@/api/client'
-import { createEntityAdapter } from '@reduxjs/toolkit'
+import { createEntityAdapter, createSelector, EntityState } from '@reduxjs/toolkit'
 import { selectCurrentUserID } from '../auth/authSlice'
+import { apiSlice } from '../api/apiSlice'
 
 // Define a TS type for the data we'll be using
 export interface User {
@@ -11,41 +10,38 @@ export interface User {
 }
 
 const usersAdapter = createEntityAdapter<User>()
-
 const initialState = usersAdapter.getInitialState()
 
-// Create the slice and pass in the initial state
-const usersSlice = createAppSlice({
-    name: 'users',
-    initialState,
-    reducers: create => {
-        return {
-            fetchUsers: create.asyncThunk(
-                async () => {
-                    const response = await client.get<User[]>('fakeApi/users')
-                    return response.data
-                },
-                {
-                    fulfilled: usersAdapter.setAll
-                }
-            )
-        }
-    },
+export const apiSliceWithUsers = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        getUsers: builder.query<EntityState<User, string>, void>({
+            query: () => '/users',
+            transformResponse(res: User[]) {
+                // Create a normalized state object containing all the user items
+                return usersAdapter.setAll(initialState, res)
+            }
+        })
+    })
 })
 
-// Export the auto-generated action creator with the same name
-export const { fetchUsers } = usersSlice.actions
+export const { useGetUsersQuery } = apiSliceWithUsers
 
-// Export the generated reducer function
-export default usersSlice.reducer
+// Calling `someEndpoint.select(someArg)` generates a new selector that will return
+// the query result object for a query with those parameters.
+// To generate a selector for a specific query argument, call `select(theQueryArg)`.
+// In this case, the users query has no params, so we don't pass anything to select()
+export const selectUsersResult = apiSliceWithUsers.endpoints.getUsers.select()
 
-export const {
-    selectAll: selectAllUsers,
-    selectById: selectUserById
-} = usersAdapter.getSelectors((state: RootState) => state.users)
+const selectUsersData = createSelector(
+    selectUsersResult,
+    result => result.data ?? initialState
+)
 
 export const selectCurrentUser = (state: RootState) => {
-    const currentUserId = selectCurrentUserID(state) as string
-    if (!currentUserId) {return}
-    return selectUserById(state, currentUserId)
+    const currentUserID = selectCurrentUserID(state)
+    if (currentUserID) {
+        return selectUserById(state, currentUserID)
+    }
 }
+
+export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors(selectUsersData)
