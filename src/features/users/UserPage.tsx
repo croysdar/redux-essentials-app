2bff9db71fb717f495a9ff10998b00b744881f65
Rebecca <rebecca@croysdale.net>
2024-09-17 12:35:40 -0700

    Fixes user page's posts to come from apiSlice

--- src/features/users/UserPage.tsx
+++ src/features/users/UserPage.tsx
@@ -2,18 +2,34 @@ import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 
 import { useAppSelector } from '@/app/hooks'
-import { selectPostsByUser } from '@/features/posts/postsSlice'
 
 import { selectUserById } from './usersSlice'
+import { Post, useGetPostsQuery } from '../api/apiSlice'
+import { BaseQueryFn, TypedUseQueryStateResult } from '@reduxjs/toolkit/query/react'
+import { createSelector } from '@reduxjs/toolkit'
+
+// Create a TS type that represents "the result value passed
+// into the `selectFromResult` function for this hook"
+type GetPostSelectFromResultsArg = TypedUseQueryStateResult<Post[], unknown, BaseQueryFn>
+
+const selectPostsForUser = createSelector(
+  (res: GetPostSelectFromResultsArg) => res.data,
+  (res: GetPostSelectFromResultsArg, userId: string) => userId,
+  (data, userId) => data?.filter(post => post.user === userId)
+)
 
 export const UserPage = () => {
   const { userId } = useParams()
 
   const user = useAppSelector(state => selectUserById(state, userId!))
 
-  const postsForUser = useAppSelector(state =>
-    selectPostsByUser(state, userId!)
-  )
+  // selectFromResult is good for situations like this where you want a filtered list
+  const { postsForUser } = useGetPostsQuery(undefined, {
+    selectFromResult: result => ({
+      ...result,
+      postsForUser: selectPostsForUser(result, userId!)
+    }),
+  })
 
   if (!user) {
     return (
@@ -23,7 +39,7 @@ export const UserPage = () => {
     )
   }
 
-  const postTitles = postsForUser.map(post => (
+  const postTitles = postsForUser?.map(post => (
     <li key={post.id}>
       <Link to={`/posts/${post.id}`}>{post.title}</Link>
     </li>
